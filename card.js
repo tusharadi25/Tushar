674fb165172f6f274f03ab4726796968170ead75
tusharadi25 <tusharadi25@outlook.com>
2018-12-20 18:47:40 +0530

    a

---
+++ card.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+// Pull in our modules
+var chalk = require('chalk')
+var boxen = require('boxen')
+
+// Define options for Boxen
+let options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+// Text + chalk definitions
+let data = {
+  'name': chalk.white('Tushar Adivarekar/'),
+  'handle': chalk.cyan('Adi25'),
+  'work': chalk.white('Student in Xavier Institute of Engineering '),
+  'twitter': chalk.cyan('https://twitter.com/tusharadi25'),
+  'github': chalk.cyan('https://github.com/tusharadi25'),
+  'linkedin': chalk.cyan('https://www.linkedin.com/in/tusharadi25/'),
+  'Instagram': chalk.cyan('https://www.instagram.com/tushar_adivarekar/'),
+  'npx': chalk.white('npx tusharadi'),
+  'labelWork': chalk.white.bold('      Work:'),
+  'labelTwitter': chalk.white.bold('   Twitter:'),
+  'labelGitHub': chalk.white.bold('    GitHub:'),
+  'labelLinkedIn': chalk.white.bold('  LinkedIn:'),
+  'labelWeb': chalk.white.bold(' Instagram:'),
+  'labelCard': chalk.white.bold('      Card:')
+}
+
+// Actual strings we're going to output
+var newline = '\n'
+var heading = `${data.name} ${data.handle}`
+var working = `${data.labelWork}  ${data.work}`
+var twittering = `${data.labelTwitter}  ${data.twitter}`
+var githubing = `${data.labelGitHub}  ${data.github}`
+var linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+var instagram = `${data.labelWeb}  ${data.Instagram}`
+var carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+let output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + instagram + newline + newline + carding
+
+console.log(chalk.green(boxen(output, options)))

fa4b495ea83fb35b3c4bda243786c1b1876d022a
Tushar Adivarekar <tusharadi25@outlook.com>
2024-12-06 14:48:35 +0530

    Update card.js

--- card.js
+++ card.js
@@ -14,15 +14,13 @@ let options = {
 // Text + chalk definitions
 let data = {
   'name': chalk.white('Tushar Adivarekar/'),
-  'handle': chalk.cyan('Adi25'),
-  'work': chalk.white('Student in Xavier Institute of Engineering '),
-  'twitter': chalk.cyan('https://twitter.com/tusharadi25'),
+  'handle': chalk.cyan('tusharadi25'),
+  'work': chalk.white('Developer at Nutanix'),
   'github': chalk.cyan('https://github.com/tusharadi25'),
   'linkedin': chalk.cyan('https://www.linkedin.com/in/tusharadi25/'),
   'Instagram': chalk.cyan('https://www.instagram.com/tushar_adivarekar/'),
   'npx': chalk.white('npx tusharadi'),
   'labelWork': chalk.white.bold('      Work:'),
-  'labelTwitter': chalk.white.bold('   Twitter:'),
   'labelGitHub': chalk.white.bold('    GitHub:'),
   'labelLinkedIn': chalk.white.bold('  LinkedIn:'),
   'labelWeb': chalk.white.bold(' Instagram:'),
@@ -33,13 +31,12 @@ let data = {
 var newline = '\n'
 var heading = `${data.name} ${data.handle}`
 var working = `${data.labelWork}  ${data.work}`
-var twittering = `${data.labelTwitter}  ${data.twitter}`
 var githubing = `${data.labelGitHub}  ${data.github}`
 var linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 var instagram = `${data.labelWeb}  ${data.Instagram}`
 var carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-let output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + instagram + newline + newline + carding
+let output = heading + newline + newline + working + newline + githubing + newline + linkedining + newline + instagram + newline
 
 console.log(chalk.green(boxen(output, options)))
